0e73ac3f1da461d64399d4a078c1b7b1002470d5
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-03-29 14:56:34 +0100

    pushed audio to storage

--- InMyZoneApp/Controller/RecordViewController.swift
+++ InMyZoneApp/Controller/RecordViewController.swift
@@ -13,13 +13,14 @@ import FirebaseStorage
 import FirebaseAuth
 
 class RecordViewController: UIViewController, AVAudioRecorderDelegate {
-
+    
     @IBOutlet weak var recordButtonImage: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var descriptionTextField: UITextField!
     
     var db: Firestore!
     var auth: Auth!
+    var storage: Storage!
     
     var recordingSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
@@ -34,6 +35,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
         
         db = Firestore.firestore()
         auth = Auth.auth()
+        storage = Storage.storage()
         
         Design.shared.setBackground(view: view)
         
@@ -56,20 +58,20 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
         itemref.addDocument(data: post.toAny()) { (error) in
             if error != nil {
                 print("Error when performing add: \(error?.localizedDescription) ")
-                }   else {
+            }   else {
                 print("Succes adding") 
-                }
+            }
         }
         
         
-//        let db = Firestore.firestore().collection("posts")
-//            db.addDocument(data: post.toAny()) { (error) in
-//            if error != nil{
-//                print("Error when performing add: \(error?.localizedDescription) ")
-//            }   else {
-//                print("Succes adding")
-//                }
-//        }
+        //        let db = Firestore.firestore().collection("posts")
+        //            db.addDocument(data: post.toAny()) { (error) in
+        //            if error != nil{
+        //                print("Error when performing add: \(error?.localizedDescription) ")
+        //            }   else {
+        //                print("Succes adding")
+        //                }
+        //        }
         
         
         
@@ -80,7 +82,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
     
     @IBAction func playButton(_ sender: UIButton) {
         do {
-        audioPlayer = try AVAudioPlayer(contentsOf: getFileURL() as URL)
+            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL() as URL)
         } catch let error as NSError {
             print("Error: \(error.localizedDescription)")
         }
@@ -103,7 +105,7 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
                 audioRecorder = try AVAudioRecorder(url: fileName, settings: settings)
                 audioRecorder.delegate = self
                 audioRecorder.record()
-            
+                
                 let image = UIImage(named: "stop-96")
                 recordButtonImage.setImage(image, for: .normal)
             }   catch {
@@ -112,6 +114,28 @@ class RecordViewController: UIViewController, AVAudioRecorderDelegate {
         }   else {
             //Stopping Audio Recording
             audioRecorder.stop()
+            
+            let uuid = NSUUID().uuidString
+            let soundRef = storage.reference().child("images/\(uuid)")
+            
+            let uploadTask = soundRef.putFile(from: audioRecorder.url, metadata: nil) {
+                metadata, error in
+                if let metadata = metadata {
+                    print("upload")
+                    do {
+                        let url = self.getFileURL()
+                        try FileManager.default.removeItem(at: url)
+                    } catch {}
+                } else {
+                    print("error")
+                }
+                
+                
+                
+            }
+            
+            
+            
             print("AudioRecorder url is: \(audioRecorder.url)")
             
             print("url String is: \(String(describing: urlString))")
