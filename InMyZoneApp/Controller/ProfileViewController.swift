1f73734fe3e2827b717ae2f06ec51dfe45b0f4a1
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-04-17 14:56:44 +0200

    Started profile Storyboard

--- InMyZoneApp/Controller/ProfileViewController.swift
+++ InMyZoneApp/Controller/ProfileViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var aboutMeTextView: UITextView!
+    @IBOutlet weak var followersLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    
+    var isTapped = false
+    var db = Firestore.firestore()
+    var profilePicUrl: String?
+    var username: String?
+    var userListener: ListenerRegistration?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         Design.shared.setBackground(view: view)
+        
+        getUserprofile()
+        getAboutMeText()
+        
+        Design.shared.adjustUITextViewHeight(arg: aboutMeTextView)
+        
+        profilePicture.layer.cornerRadius = profilePicture.frame.height/2
+        profilePicture.clipsToBounds = true
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -28,5 +50,81 @@ class ProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        if aboutMeTextView.isEditable == false {
+            aboutMeTextView.isEditable = true
+        } else {
+            aboutMeTextView.isEditable = false
+            let uid = Auth.auth().currentUser?.uid
+            let postRef = db.collection("users").document(uid!)
+            postRef.setData(["aboutMe": aboutMeTextView.text], merge: true)
+            aboutMeTextView.text = nil
+            
+            getAboutMeText()
+        }
+    }
+    
+    func getAboutMeText () {
+        let uid = Auth.auth().currentUser?.uid
+        let postRef = db.collection("users").document(uid!)
+        postRef.getDocument { (snapshot, error) in
+            if error != nil {
+                print("error: \(error)")
+            } else {
+                print("SUCCESS")
+                if let snapshot = snapshot {
+                    let data = snapshot.data()
+                    self.aboutMeTextView.text = data!["aboutMe"] as? String ?? ""
+                }
+            }
+        }
+    }
+    
+    func getUserprofile() {
+        print("!!!!!")
+        let uid = Auth.auth().currentUser?.uid
+        let postRef = db.collection("users").document(uid!)
+        print("UID IS!!!!!!!: \(uid)")
+        postRef.getDocument { (snapshot, error) in
+            if error != nil {
+                print("ERROR \(error)")
+            } else {
+                print("SUCCESS")
+                if let snapshot = snapshot {
+                    let data = snapshot.data()
+                    self.usernameLabel.text = data!["username"] as? String ?? ""
+                    self.profilePicUrl = data!["photoUrl"] as? String ?? ""
+                    self.profilePicture.sd_setImage(with: URL(string: self.profilePicUrl!), placeholderImage: UIImage(named: "profilePlaceholder"))
+                }
+            }
+        }
 
+        
+//        guard let auth = Auth.auth().currentUser else {return}
+//        let uid = Auth.auth().currentUser?.uid
+//        let userRef = db.collection("users").document("\(uid)")
+//        userRef.getDocument { (document, error) in
+//            if error != nil {
+//                print("Error getting doc : \(String(describing: error))")
+//            }   else {
+//                if let document = document {
+//                    let docData = document.data()
+//                    self.username = docData?["username"] as? String ?? ""
+//                    self.usernameLabel.text = self.username
+//                    self.profilePicUrl = docData?["photoUrl"] as? String ?? ""
+//                    self.profilePicture.sd_setImage(with: URL(string: self.profilePicUrl!), placeholderImage: UIImage(named: "profilePlaceholder"))
+//                }
+//            }
+//        }
+    }
+    
+    
+    
+    
+//    func changeBarButtonToSave() {
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: nil)
+//    }
+//    func changeBarButtonToEdit() {
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: nil)
+//    }
 }
